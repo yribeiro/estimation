a837712bd31b6b9a5848d339939c22a58d13b866
Yohahn Ribeiro <yohahnribeiro29@gmail.com>
2021-07-03 14:36:17 +0100

    Updated the utils file to plot the points correctly.

--- utils.py
+++ utils.py
@@ -2,14 +2,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_data(estimates, measurements, truth):
+def plot_data(estimates, predictions, measurements, truth):
+    # all data is from t1 onwards .. except truth
     estimates = np.array(estimates)
     measurements = np.array(measurements)
+    predictions = np.array(predictions)
     truth = np.array(truth)
+    time = np.array(range(1, len(measurements)+1))
 
     plt.plot(truth, ls="-", color="k", label="Truth")
-    plt.plot(estimates, ls=":", color="b", label="Estimates")
-    plt.scatter(np.array([range(len(truth))]), measurements, marker="o", color="k", label="Measurements")
+    plt.plot(time, estimates, ls=":", color="b", label="Estimates")
+    plt.scatter(time, measurements, marker="o", color="k", label="Measurements")
+    plt.scatter(time, predictions, marker="^", color="r", label="Predictions")
     plt.grid()
     plt.legend()
     plt.show()
