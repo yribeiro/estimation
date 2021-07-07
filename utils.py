18a156a562025b865168c5192261848a3653f752
Yohahn Ribeiro <yohahn.ribeiro@baesystems.com>
2021-07-07 11:44:33 +0100

    Implemented a dog simulation to move dog along hallway - probabilistically.

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from math import sqrt
 from typing import Optional, Tuple
 
 import numpy as np
@@ -33,3 +34,34 @@ def plot_bars(arr: np.ndarray, title: Optional[str] = None, ylim: Optional[Tuple
     plt.grid()
     if show:
         plt.show(block=False)
+
+
+class DogSimulation(object):
+    def __init__(self, x0=0, velocity=1,
+                 measurement_var=0.0,
+                 process_var=0.0):
+        """ x0 : initial position
+            velocity: (+=right, -=left)
+            measurement_var: variance in measurement m^2
+            process_var: variance in process (m/s)^2
+        """
+        self.x = x0
+        self.velocity = velocity
+        self.meas_std = sqrt(measurement_var)
+        self.process_std = sqrt(process_var)
+
+    def move(self, dt=1.0):
+        """Compute new position of the dog in dt seconds."""
+        # multiplying a standard normal distribution sample by std deviation creates mean 0 and std Gaussian
+        dx = self.velocity + np.random.randn() * self.process_std
+        self.x += dx * dt
+
+    def sense_position(self):
+        """ Returns measurement of new position in meters."""
+        measurement = self.x + np.random.randn() * self.meas_std
+        return measurement
+
+    def move_and_sense(self):
+        """ Move dog, and return measurement of new position in meters"""
+        self.move()
+        return self.sense_position()
